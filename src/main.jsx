d115105be32a1216565857fbba550c38d7dffea8
hasslefreepay <hasslefreepay@gmail.com>
2025-01-28 01:47:05 -0500

    Deploy to GitHub Pages

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Pagina2 from "./pagina2.jsx";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />}/>
+              <Route path="/pagina2" element={<Pagina2/>}/>
+            </Routes>
+      </BrowserRouter>
+  </StrictMode>,
+)
